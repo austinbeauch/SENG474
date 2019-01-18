f6946391e456d4fa9f617cf0109a58cb3d7b2a2d
austin <austinbeauch@gmail.com>
2019-01-18 11:15:50 -0800

    Replace pandas with builtins

--- asn1/src/P1.py
+++ asn1/src/P1.py
@@ -1,18 +1,19 @@
+import re
 import time
 import csv
 from copy import deepcopy
-import numpy
-import pandas as pd
-
-from similarities import get_jaccard_sim
+from pprint import pprint
 
 
-def matrix(df, eps):
-    d = df.to_dict()['question']
-    # d2 = deepcopy(d)
-	
-	
+# https://towardsdatascience.com/overview-of-text-similarity-metrics-3397c4601f50
+def get_jaccard_sim(str1, str2): 
+    a = set(str1.split()) 
+    b = set(str2.split())
+    c = a.intersection(b)
+    return float(len(c)) / (len(a) + len(b) - len(c))
 
+
+def matrix(d, eps):
     sims = {}
     start = time.time()
     for i in d:
@@ -31,6 +32,7 @@ def matrix(df, eps):
     print("Loop time:", time.time() - start)
     return sims
 
+    
 def make_dict(d, n):
     with open("question_sim_{}k.tsv".format(n), "w+") as f:
         f.write("qid\tsimilar-qids\n")
@@ -42,10 +44,22 @@ def make_dict(d, n):
 if __name__ == "__main__":
     n = "4"
     fpath = "../data/question_{}k.tsv".format(n)
-    data = pd.read_table(fpath, index_col=0)
-
+    lines = [line.rstrip("\n") for line in open(fpath, encoding="utf8")]
+    
+    words = []
+    data = {}
+    word_freq = {}
+    for i in lines[1:]:
+        try:
+            qid, question = i.split("\t")
+            question = re.sub(r'[^a-zA-Z0-9 ]+', r'', question)
+            question = question.lower()
+            data[qid] = question
+        except ValueError:
+            continue
+    pprint(data)
+    
     e = 0.6
     
     s = matrix(data, e)
-    print(numpy.min(np.array(s.values().astype(int))))
     make_dict(s, n)
--- asn1/src/similarities.py
+++
@@ -1,6 +0,0 @@
-# https://towardsdatascience.com/overview-of-text-similarity-metrics-3397c4601f50
-def get_jaccard_sim(str1, str2): 
-    a = set(str1.split()) 
-    b = set(str2.split())
-    c = a.intersection(b)
-    return float(len(c)) / (len(a) + len(b) - len(c))
