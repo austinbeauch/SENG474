7983c0bb804dbae96626b7a9228bac6065e987f0
austin <austinbeauch@gmail.com>
2019-01-21 10:51:01 -0800

    Create ground set of all words

--- asn1/src/P2.py
+++ asn1/src/P2.py
@@ -1,64 +1,68 @@
-from fnv import *
 import uuid
-import numpy
+import time
 import re
+from pprint import pprint
+from random import shuffle
 
 
-p =  15373875993579943603
+import fnv
+
+p =	 15373875993579943603
+x = 0.6
+s = 14
+r = 6
+
+
+def get_qid_question(line):
+	qid, question = line.split("\t")
+	question = re.sub(r'[^a-zA-Z0-9 ]+', r'', question)
+	question = question.lower()
+	return qid, question
+
+
+def ground_set(f):
+	u = set()
+	lines = [line.rstrip("\n")for line in open(f, encoding = "utf8")]
+	for line in lines[1:]:
+			try:
+				qid, question = get_qid_question(line)
+				#for word in question.split():
+				u.update(question.split())
+			except ValueError:
+				continue
+	return u
+
 def testing(f):
+	a = [uuid.uuid4().int & (1<<64)-1 for i in range(0,14)]
+	b = [uuid.uuid4().int & (1<<64)-1 for i in range(0,14)]
+	U = ground_set(f)
 
 	lines = [line.rstrip("\n")for line in open(f, encoding = "utf8")]
 	m = 1
 	for line in lines[1:]:
 		try:
-			qid, question = line.split("\t")
-			question = re.sub(r'[^a-zA-Z0-9 ]+', r'', question)
-			question = question.lower()
+			qid, question = get_qid_question(line)
+
+			u = []  # TODO: Make ground set the full set of all words
 			for word in question.split():
-				# word = word.encode("utf-8")
-				hashcode = hash(word.encode("utf-8"), bits=64)
-				
-				for i in range(0, 14):
-					k = hash_function(a[i], b[i], hashcode)
-					if k in B[i]:
-						B[i][k].append(qid)	
-					else:
-						B[i][k] = [qid, word]	
-				#if word == "Quora":
-					#print(type(question))
-					#question = question.encode("utf-8")
-					#hashcode = hash(question, bits=64) # fnv.fnv_1a is a default algorithm
-					#print(hashcode)
-			#print(question)
+				hashcode = fnv.hash(word.encode("utf-8"), bits=64)
+				u.append(hashcode)
+			
+			# pi_i = [[i for i in range(len(u))] for _ in range(6)]
+			# for i in pi_i:
+			# 	shuffle(i)
+			# pprint(pi_i)
+			# exit()
+
 		except ValueError:
 			continue
 	return m
 
-def hash_function(a,b,fnvh):
-	hf = (a*fnvh + b)% p
-	return(hf)
+def hash_function(a, b, fnvh):
+	hf = (a * fnvh + b) % p
+	return hf
 
 if __name__ == "__main__":
 	n = "4"
 	fpath = "../data/question_{}k.tsv".format(n)
-	#data = pd.read_table(fpath, index_col=0)
-	a = [uuid.uuid4().int & (1<<64)-1 for i in range(0,14)]
-	b = [uuid.uuid4().int & (1<<64)-1 for i in range(0,14)]
-	#print(a)
-	#print(b)
-	rand64 = uuid.uuid4().int & (1<<64)-1
-	#print("a = 2, b = 3, fnvh = 10")
-	#print(hash_function(a[0],b[0],100))
-	
-	B = [{} for i in range(0,14)]
-	#for i in range(0,14):
-	#B[2][2] = ["Hello World"]
-	#print(B[2])
-	#B[2][2].append("wee")
-	#print(B[2])
 	m = testing(fpath)
-	#print(B[1].keys())
-	print(min(B[1].keys()))
-	print(B[1][min(B[1].keys())])
-	print("What")
-	print(m in B[1].keys())
