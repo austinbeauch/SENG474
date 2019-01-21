45c0085840366fea2a78f24419fb365b222a0a9e
austin <austinbeauch@gmail.com>
2019-01-21 11:05:24 -0800

    Add timeit method and refactor line handling

--- asn1/src/P2.py
+++ asn1/src/P2.py
@@ -4,14 +4,28 @@ import re
 from pprint import pprint
 from random import shuffle
 
-
 import fnv
 
-p =	 15373875993579943603
+
+p = 15373875993579943603
 x = 0.6
 s = 14
 r = 6
 
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f s' % \
+                  (method.__name__, (te - ts)))
+        return result
+    return timed
 
 def get_qid_question(line):
 	qid, question = line.split("\t")
@@ -19,40 +33,30 @@ def get_qid_question(line):
 	question = question.lower()
 	return qid, question
 
-
-def ground_set(f):
+@timeit
+def ground_set(lines):
+	"""Creates ground set of all unique words given in lines"""
 	u = set()
-	lines = [line.rstrip("\n")for line in open(f, encoding = "utf8")]
 	for line in lines[1:]:
 			try:
 				qid, question = get_qid_question(line)
-				#for word in question.split():
 				u.update(question.split())
 			except ValueError:
 				continue
-	return u
+	return sorted(list(u))
 
-def testing(f):
-	a = [uuid.uuid4().int & (1<<64)-1 for i in range(0,14)]
-	b = [uuid.uuid4().int & (1<<64)-1 for i in range(0,14)]
-	U = ground_set(f)
+@timeit
+def main(lines):
+	a = [uuid.uuid4().int & (1<<64)-1 for i in range(0, s)]
+	b = [uuid.uuid4().int & (1<<64)-1 for i in range(0, s)]
+	U = ground_set(lines)
 
-	lines = [line.rstrip("\n")for line in open(f, encoding = "utf8")]
 	m = 1
 	for line in lines[1:]:
 		try:
 			qid, question = get_qid_question(line)
-
-			u = []  # TODO: Make ground set the full set of all words
 			for word in question.split():
 				hashcode = fnv.hash(word.encode("utf-8"), bits=64)
-				u.append(hashcode)
-			
-			# pi_i = [[i for i in range(len(u))] for _ in range(6)]
-			# for i in pi_i:
-			# 	shuffle(i)
-			# pprint(pi_i)
-			# exit()
 
 		except ValueError:
 			continue
@@ -63,6 +67,7 @@ def hash_function(a, b, fnvh):
 	return hf
 
 if __name__ == "__main__":
-	n = "4"
+	n = "150"
 	fpath = "../data/question_{}k.tsv".format(n)
-	m = testing(fpath)
+	l = [line.rstrip("\n")for line in open(fpath, encoding = "utf8")]
+	m = main(l)
